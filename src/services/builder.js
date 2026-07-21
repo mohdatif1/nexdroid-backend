a6ccb1b22ab893cd01c487efc9a4ce6bfc64b714
mohdatif1 <samirazmi133@gmail.com>
2026-07-21 11:32:08 +0530

    Update builder.js

--- src/services/builder.js
+++ src/services/builder.js
@@ -455,7 +455,8 @@ function generateActivityLayout() {
   return `<?xml version="1.0" encoding="utf-8"?>
 <RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
     android:layout_width="match_parent"
-    android:layout_height="match_parent">
+    android:layout_height="match_parent"
+    android:fitsSystemWindows="true">
 
     <WebView
         android:id="@+id/webview"
